721c1603c9e86676f7cef5ea070c82c78b87e2c0
Limon24041 <it24041@mbstu.ac.bd>
2026-05-01 09:49:26 +0600

    Create Example1.java

---
+++ Day_8/Example1.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class StudentList {
+    public static void main(String[] args) {
+        ArrayList<String> students = new ArrayList<>();
+
+        students.add("Milon");
+        students.add("Jibon");
+        students.add("Apon");
+
+        System.out.println(students);
+    }
+}
