789b6427060a1ced9f131092edd253972c99cb80
Limon24041 <it24041@mbstu.ac.bd>
2026-05-02 21:11:19 +0600

    Create Example2.java

---
+++ Day_13/Example2.java
@@ -0,0 +1,14 @@
+import java.util.TreeSet;
+
+public class TreeSetExample {
+    public static void main(String[] args) {
+
+        TreeSet<String> names = new TreeSet<>();
+
+        names.add("Milon");
+        names.add("Jibon");
+        names.add("Noyon");
+
+        System.out.println(names);
+    }
+}
