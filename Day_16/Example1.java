aff3d3d0e59ece9254fbcd88f48a48efc0ccc6bd
Limon24041 <it24041@mbstu.ac.bd>
2026-05-07 10:03:54 +0600

    Create Example1.java

---
+++ Day_16/Example1.java
@@ -0,0 +1,21 @@
+import java.io.FileInputStream;
+
+public class Main {
+    public static void main(String[] args) {
+
+        try {
+            FileInputStream fin = new FileInputStream("data.txt");
+
+            int i;
+
+            while((i = fin.read()) != -1) {
+                System.out.print((char)i);
+            }
+
+            fin.close();
+
+        } catch(Exception e) {
+            System.out.println(e);
+        }
+    }
+}
